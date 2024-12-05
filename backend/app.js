dd1aea126b942e9b83d00cae96e7cc4b2a80c998
Shwetakshi Priya <110232435+shwetakshipriya@users.noreply.github.com>
2024-12-05 15:55:12 +0530

    user login done

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,20 @@
-
-const dotenv = require('dotenv');
-
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const connectToDb = require("./db/db");
+const userRoutes = require("./routes/user.routes");
 
-const cors = require('cors');
+connectToDb();
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
+
+app.use('/users', userRoutes);
 
 module.exports = app;
