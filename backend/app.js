1995a11595424bf1cd34129c504467407e30cdee
Shwetakshi Priya <110232435+shwetakshipriya@users.noreply.github.com>
2024-12-06 01:10:47 +0530

    register captain done

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,9 @@ const cors = require("cors");
 const app = express();
 const connectToDb = require("./db/db");
 const userRoutes = require("./routes/user.routes");
+const captainRoutes = require("./routes/captain.routes");
 const cookieParser = require("cookie-parser");
+
 
 connectToDb();
 app.use(cors());
@@ -18,5 +20,6 @@ app.get("/", (req, res) => {
 });
 
 app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
 
 module.exports = app;
