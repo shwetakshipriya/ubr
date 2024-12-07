150bb67dff12097231845941ce8bab7d85f5c22d
Shwetakshi Priya <110232435+shwetakshipriya@users.noreply.github.com>
2024-12-07 18:45:46 +0530

    login and signup done

---
+++ frontend/src/context/userContext.jsx
@@ -0,0 +1,24 @@
+import { createContext,useState  } from "react"
+
+
+export const UserDataContext = createContext({})
+
+const userContext = ({children}) => {
+
+ const [user, setUser] = useState({
+    email:'',
+    fullName:{
+        firstName:'',
+        lastName:''
+    }
+ })
+  return (
+    <div>
+        <UserDataContext.Provider value={[user,setUser]}>
+            {children}
+        </UserDataContext.Provider>
+    </div>
+  )
+}
+
+export default userContext
