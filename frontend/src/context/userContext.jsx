f538a6e2693ba1eb3009e400f5385bb84e39ee11
Shwetakshi Priya <110232435+shwetakshipriya@users.noreply.github.com>
2025-01-05 02:05:08 +0530

    authofuserandcaptain

--- frontend/src/context/userContext.jsx
+++ frontend/src/context/userContext.jsx
@@ -1,24 +1,25 @@
-import { createContext,useState  } from "react"
+import React, { createContext, useState } from 'react'
+
+export const UserDataContext = createContext()
 
 
-export const UserDataContext = createContext({})
+const UserContext = ({ children }) => {
 
-const userContext = ({children}) => {
+    const [ user, setUser ] = useState({
+        email: '',
+        fullName: {
+            firstName: '',
+            lastName: ''
+        }
+    })
 
- const [user, setUser] = useState({
-    email:'',
-    fullName:{
-        firstName:'',
-        lastName:''
-    }
- })
-  return (
-    <div>
-        <UserDataContext.Provider value={[user,setUser]}>
-            {children}
-        </UserDataContext.Provider>
-    </div>
-  )
+    return (
+        <div>
+            <UserDataContext.Provider value={{ user, setUser }}>
+                {children}
+            </UserDataContext.Provider>
+        </div>
+    )
 }
 
-export default userContext
+export default UserContext
--- frontend/src/pages/Captainlogin.jsx
+++ frontend/src/pages/Captainlogin.jsx
@@ -1,15 +1,37 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { CaptainDataContext } from '../context/CapatainContext'
+
 const Captainlogin = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [captainData, setCaptainData] = useState('')
-  const submitHandler = (e) => {
+
+  const [ email, setEmail ] = useState('')
+  const [ password, setPassword ] = useState('')
+
+  const { captain, setCaptain } = React.useContext(CaptainDataContext)
+  const navigate = useNavigate()
+
+
+
+  const submitHandler = async (e) => {
     e.preventDefault();
-    setCaptainData({
-      email:email,
+    const captain = {
+      email: email,
       password
-    })
+    }
+
+    const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/captains/login`, captain)
+
+    if (response.status === 200) {
+      const data = response.data
+
+      setCaptain(data.captain)
+      localStorage.setItem('token', data.token)
+      navigate('/captain-home')
+
+    }
+
     setEmail('')
     setPassword('')
   }
@@ -17,6 +39,7 @@ const Captainlogin = () => {
     <div className='p-7 h-screen flex flex-col justify-between'>
       <div>
         <img className='w-20 mb-3' src="https://www.svgrepo.com/show/505031/uber-driver.svg" alt="" />
+
         <form onSubmit={(e) => {
           submitHandler(e)
         }}>
@@ -31,7 +54,9 @@ const Captainlogin = () => {
             type="email"
             placeholder='email@example.com'
           />
+
           <h3 className='text-lg font-medium mb-2'>Enter Password</h3>
+
           <input
             className='bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'
             value={password}
@@ -41,19 +66,22 @@ const Captainlogin = () => {
             required type="password"
             placeholder='password'
           />
+
           <button
             className='bg-[#111] text-white font-semibold mb-3 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base'
           >Login</button>
+
         </form>
         <p className='text-center'>Join a fleet? <Link to='/captain-signup' className='text-blue-600'>Register as a Captain</Link></p>
       </div>
       <div>
         <Link
-        to='/login'
+          to='/login'
           className='bg-[#d5622d] flex items-center justify-center text-white font-semibold mb-5 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base'
         >Sign in as User</Link>
       </div>
     </div>
   )
 }
+
 export default Captainlogin
